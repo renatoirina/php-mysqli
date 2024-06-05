8f775e27daeae48f325250d2fa5d53f960c4bbd4
renatoirina <irinarenato793@gmail.com>
2024-06-05 10:40:43 +0200

    feat: used fetch_object for print the rows

--- index.php
+++ index.php
@@ -20,6 +20,20 @@ try {
 $sql = "SELECT * FROM `departments`";
 $result = $connection->query($sql);
 
-var_dump($result);
+if($result && $result->num_rows > 0) {
+
+    //prelevare i risultati di ogni riga ( eseguire fetch_object tante volte quante righe ho )
+    
+    $row1 = $result->fetch_object();
+    $row2 = $result->fetch_object();
+    $row3 = $result->fetch_object();
+
+    var_dump($row1);
+    var_dump($row2);
+    var_dump($row3);
+
+    var_dump($result);
+
+}
 
 ?>
