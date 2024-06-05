08790f0931f60139c885752c8565d0fe57b97756
renatoirina <irinarenato793@gmail.com>
2024-06-05 10:28:48 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+define("DB_SERVER", "localhost");
+define("DB_USERNAME", "root");
+define("DB_PASSWORD", "root");
+define("DB_NAME", "university_db_small");
+define("DB_PORT", 3306);
+
+try {
+
+    $connection = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME, DB_PORT);
+
+} catch (Exception $e) {
+
+    echo $e->getMessage();
+    die();
+
+}
+
+var_dump($connection);
